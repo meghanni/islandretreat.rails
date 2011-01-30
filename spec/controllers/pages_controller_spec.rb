9901ab45dc1ed08ff97869b46fac3f5f4c201db2
Meghan Nicholas <meghan.nicholas@gmail.com>
2011-01-30 16:28:48 +0000

    Done with creating static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,18 +1,54 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
+  #presence of page
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+
+    #titles
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                        :content => "Island Retreat Holiday Cottage | Home")
+    end
+
   end
 
   describe "GET 'contact'" do
+
+    #presence of page
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+
+    #titles
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                        :content => "Island Retreat Holiday Cottage | Contact")
+    end
+
+  end
+
+  describe "GET 'about'" do
+
+    #presence of page
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+
+    #titles
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                        :content => "Island Retreat Holiday Cottage | About")
     end
   end
 

3820f857889d134bf786b8164e071901220e03cb
Meghan Nicholas <meghan@VMUB1004LTS02.CNDB>
2011-01-30 15:26:19 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Islandretreat::Application.config.secret_token = '7aefe48ee79c729f98bf37e214d9d4274b129fd949d60d03c709f6d402d84bfdd13df8b6065797424fd1df5df3555ea2c67913f945f3eda0fd225d0cfde392c5'
